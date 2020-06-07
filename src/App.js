c7b9a71c4f57e3e17c8dc4d98b5a4224cc136a17
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-06-07 11:57:28 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// Array of Object where the products are stored
+const products = [
+  {
+    name: "test",
+    price: 90,
+    img_url: "www.test.lf"
+  },
+  {
+    name: "test",
+    price: 90,
+    img_url: "www.test.lf"
+  },
+  {
+    name: "test",
+    price: 90,
+    img_url: "www.test.lf"
+  },
+  {
+    name: "test",
+    price: 90,
+    img_url: "www.test.lf"
+  },
+  {
+    name: "test",
+    price: 90,
+    img_url: "www.test.lf"
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>App</p>
     </div>
   );
 }
