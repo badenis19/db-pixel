47254a922222f46cd1b1a18c9f41953d394d3226
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-06-08 23:55:55 +0100

    pushing stat to locol storage with with timer working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -61,6 +61,55 @@ const App = () => {
       img_url: "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1007&q=80"
     }
   ])
+
+  const [trackingData, setTrackingData] = useState(() => {
+    const localData = localStorage.getItem('TrackingData');
+    return localData ? JSON.parse(localData) : [];
+  });
+
+  // console.log(">>", trackingData)
+
+  // declaring variables
+  let start_minutes = null;
+  let start_seconds = null;
+
+  // get the minutes on page load
+  const start = () => {
+    start_minutes = new Date().getMinutes();
+    start_seconds = new Date().getSeconds();
+    return `start ${start_minutes} + ${start_seconds}`;
+  }
+
+  start();
+
+  // get minutes before leaving the page and works out time spent on the page
+  setTimeout(() => {
+    let minutes = new Date().getMinutes();
+    let seconds = new Date().getSeconds();
+    let diff_minutes = minutes - start_minutes;
+    let diff_seconds = seconds - start_seconds;
+    let diff = `${diff_minutes}:${diff_seconds}`;
+    // console.log(diff)
+    let data = {
+      path: "path",
+      time_on_page: diff
+    }
+    trackingData.push(data)
+    console.log("::", trackingData)
+    return diff;
+  }, 3000)
+
+  setTimeout(useEffect(() => {
+    console.log(trackingData)
+    console.log("useEffect")
+    localStorage.setItem("TrackingData", JSON.stringify(trackingData))
+  }, [trackingData])
+    , 6000)
+
+
+  setTimeout(() => console.log("<<", trackingData)
+    , 3010)
+
 
   return (
     <Router>
--- src/component/Tracking.js
+++ src/component/Tracking.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import PageHeader from './PageHeader';
+import { useLocation, useHistory } from 'react-router-dom';
 
 const Tracking = () => {
+
+  const location = useLocation();
+  const history = useHistory();
+  console.log(location);
+
+  const handleGoBack = () => {
+    history.goBack();
+  }
 
   return (
     <div className="container">
       <PageHeader entity="Tracking" />
       <p className="intro">Find the tracking information below:</p>
-      <h2 className="text-center">COMING SOON..</h2>
+      <h2 className="text-center">COMING SOON..{location.pathname}</h2>
+      <button onClick={handleGoBack}>back</button>
     </div>
   )
 }
